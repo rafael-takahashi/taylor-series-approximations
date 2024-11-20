416b1d5b293253c8ae1278a12752d764ada75222
Rafael Takahashi <rafael.takahashi20@outlook.com>
2024-11-20 15:38:26 -0300

    feat: add error plotting functionality

--- opt_precision/main.c
+++ opt_precision/main.c
@@ -18,7 +18,6 @@ int main() {
     double res1 = _cos(M_PI / 4);
     double res2 = cos(M_PI / 4);
     
-
     double res1 = _sqrt(x);
     double res2 = sqrt(x);
     double error = fabs(res1 - res2);
@@ -28,10 +27,30 @@ int main() {
     printf("Error: %lf\n", error);
     */
 
+    /*
     double x = 2.0;
     long y = 13;
 
     double res = iterative_opt_pow(x, y);
 
     printf("Res: %lf\n", res);
+    */
+
+    FILE *f = fopen("plot/data.txt", "w");
+
+    if (f == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    double x, y;
+
+    for (x = -10; x <= 10; x += 0.1) {
+        y = fabs(sen(x) - sin(x));
+        fprintf(f, "%lf %lf\n", x, y); 
+    }
+
+    fclose(f);
+
+    return 0;
 }
---
+++ opt_precision/plot/plot.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+x_vals = []
+y_vals = []
+
+with open('data.txt', 'r') as f:
+    for line in f:
+        x, y = map(float, line.split())
+        x_vals.append(x)
+        y_vals.append(y)
+
+plt.plot(x_vals, y_vals)
+plt.xlabel('x')
+plt.ylabel('error')
+plt.title('Error')
+plt.savefig('plot.png')
